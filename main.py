4b903e0c5361bc865eef937a02ba45de6352673e
Ahmad Hossini <ahmadly.com@gmail.com>
2021-02-20 09:55:33 +0330

    update

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import csv
 from gtts import gTTS
 from deep_translator import GoogleTranslator
 
+VOICE_LANG = 'ca'
+SOURCE_LANG = 'en'
+DESTINATION_LANG = 'fa'
+SENTENCES_FILE = 'sentences.txt'
+SENTENCES_OUTPUT_FILE = 'sentences.txt.csv'
 
-def normalize_sentence(text: str) -> str:
+
+def sentence_normalizer(text: str) -> str:
     _sentence = text.strip().lower()
     if not _sentence.endswith('.'):
         _sentence = _sentence + '.'
@@ -12,13 +18,14 @@ def normalize_sentence(text: str) -> str:
     return _sentence
 
 
-with open('sentences.txt.csv', 'w', newline='\n') as file:
-    writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+if __name__ == '__main__':
+    with open(SENTENCES_OUTPUT_FILE, 'w', newline='\n') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
 
-    for sentence in open('sentences.txt').readlines():
-        sentence = normalize_sentence(sentence)
-        tts = gTTS(sentence, lang='en', tld='ca')
-        tts.save(f'{sentence.lower().replace(" ", "_").replace(".", "")}.mp3')
+        for sentence in open(SENTENCES_FILE).readlines():
+            sentence = sentence_normalizer(sentence)
+            tts = gTTS(sentence, lang=SOURCE_LANG, tld=VOICE_LANG)
+            tts.save(f'{sentence.lower().replace(" ", "_").replace(".", "")}.mp3')
 
-        translated = GoogleTranslator(source='en', target='fa').translate(sentence)
-        writer.writerow([sentence, translated])
+            translated = GoogleTranslator(source=SOURCE_LANG, target=DESTINATION_LANG).translate(sentence)
+            writer.writerow([sentence, translated])
